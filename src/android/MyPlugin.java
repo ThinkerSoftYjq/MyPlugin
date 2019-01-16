73197ed43caf5ac0d42f293f55f3a99f8ff0a173
Administrator <Administrator@USER-20181111IQ>
2019-01-16 16:06:42 +0800

    Initial commit

---
+++ src/android/MyPlugin.java
@@ -0,0 +1,43 @@
+package plugin_id;
+
+import android.app.Activity;
+import android.widget.Toast;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class MyPlugin extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("coolMethod")) {
+            String message = args.getString(0);
+            show(message);
+            this.coolMethod(message, callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    private void coolMethod(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+
+    //弹出对话框
+    void show(String info){
+        Activity activity = this.cordova.getActivity();
+        Toast toast = Toast.makeText(activity,info,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
